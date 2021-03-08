a6066a7b5620a2cfd4ee7ef6db47e88e7eef1cc4
Virendra patel <patelvirendra62@gmail.com>
2021-03-08 23:10:58 +0530

    users api

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const createConnection = require("./db/connection");
 const { User } = require("./models/user");
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 createConnection();
 const port = 3000;
 
@@ -20,4 +22,28 @@ app.get("/api", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+});
+
+const userRouter = express.Router();
+app.use("/api/users", userRouter);
+
+userRouter.get("/", (req, res) => {
+  User.find().then((users) => {
+    res.json({
+      users: users,
+    });
+  });
+});
+
+userRouter.post("/", (req, res) => {
+  const user = req.body;
+  console.log(user);
+
+  if (Object.keys(user).length == 0) {
+    return res.status(400).json({ error: "Request Body Required" });
+  }
+
+  User.create(user).then((user) => {
+    res.status(201).json({ user: user });
+  });
 });
